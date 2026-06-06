7672fa74f7eee26514968f1224c3cb9fe651a885
ernestoCruz05 <a2023144672@isec.pt>
2026-06-06 11:41:18 +0100

    feat: move clients

--- src/dispatch/bind_declare.h
+++ src/dispatch/bind_declare.h
@@ -35,6 +35,7 @@ void switch_layout(const Arg *arg);
 void setmfact(const Arg *arg);
 void quit(const Arg *arg);
 void moveresize(const Arg *arg);
+void move_client(const Arg *arg);
 void exchange_client(const Arg *arg);
 void exchange_stack_client(const Arg *arg);
 void killclient(const Arg *arg);
